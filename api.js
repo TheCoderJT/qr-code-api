919a77300363fa242b70be59c73efc4bd279a12e
TheCoderJT <gamerslife56@gmail.com>
2023-01-26 08:02:00 -0700

    changed port

--- api.js
+++ api.js
@@ -30,6 +30,6 @@ app.get("/qrsvg", (req, res) => {
   qr_svg.pipe(res);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || "3001", () => {
   console.log("QR code server listening on port 3000!");
 });
