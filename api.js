4ee805414bbe6a47e52dbaa976f8813236ac73f2
TheCoderJT <gamerslife56@gmail.com>
2023-01-26 08:51:16 -0700

    sdsa

--- api.js
+++ api.js
@@ -31,5 +31,5 @@ app.get("/qrsvg", (req, res) => {
 });
 
 app.listen(`${process.env.PORT ?? 3000}`, () => {
-  console.log("server listening on port 3000!");
+  console.log(`server listening on port ${process.env.PORT ?? 3000}`);
 });
